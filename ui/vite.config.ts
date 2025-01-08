33a66f87045e77e2744fe4835916dbfc50f1ef74
AndreyDiak <brodiaga_0880@mail.ru>
2025-01-08 20:27:57 +0300

    (ui): w

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,10 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: "@emotion/react"
-  }), tsconfigPaths()],
-})
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
+});
